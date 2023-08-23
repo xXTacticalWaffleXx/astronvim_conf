e06eb939530bded045d0fd7b5849a87d5d8ee5c2
Luna Aphelion <luna-aphelion@proton.me>
2023-08-23 07:40:48 +0100

    added rust tools

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,6 +1,7 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
+  "simrat39/rust-tools.nvim"
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
